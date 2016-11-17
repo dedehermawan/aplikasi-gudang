120e51e95202111f158b56a4ccd23c7c1e94687e
dedehermawan <alidefit@gmail.com>
2016-11-17 09:12:39 +0700

    setup spring boot

---
+++ src/main/java/com/hermawan/dede/gudang/AplikasiGudangApplication.java
@@ -0,0 +1,12 @@
+package com.hermawan.dede.gudang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiGudangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiGudangApplication.class, args);
+	}
+}
