63929b83e02e5ecd3d90d55beffa2248dd5b3577
dedehermawan <alidefit@gmail.com>
2016-11-22 15:29:46 +0700

    custom login dan logout page dengan thymeleaf

--- src/main/java/com/hermawan/dede/gudang/controller/HaloController.java
+++ src/main/java/com/hermawan/dede/gudang/controller/HaloController.java
@@ -3,19 +3,17 @@ package com.hermawan.dede.gudang.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+@Controller
 public class HaloController {
 
     @RequestMapping("/halo")
-    public Map<String, Object> halo(@RequestParam(required = false)String nama) {
-        Map<String, Object> hasil = new HashMap<String, Object>();
-        hasil.put("waktu ", new Date());
-        hasil.put("nama", nama);
-        return hasil;
+    public void haloHtml(@RequestParam(required = false) String nama, Model model) {
+        model.addAttribute("waktu", new Date());
+        model.addAttribute("nama", nama);
     }
 }
